88c6060d0f8a031a929727fda2e3067724379c9c
e <849226440@qq.com>
2019-04-21 20:18:32 +0800

    "第二"

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker run -it --rm -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven mvn clean install
 cp mall-0.0.1-SNAPSHOT.jar app.jar
-CMD ["java","-jar","/app.jar"]
+docker run -d -p 8081:8081 --name springboot java java -jar /"$(pwd)"/app.jar
