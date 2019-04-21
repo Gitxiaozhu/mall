faac52a8cd72ed8c11c9d97efd1a77fabc90c8b1
xiaozhuchen <34114482+Gitxiaozhu@users.noreply.github.com>
2019-04-21 13:37:25 +0800

    Update dockerfile.sh

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ilex
+#!/bin/bash
 docker run --rm -v SpringbootTest3:/user/src/java -w /user/src/java maven mvn clean package
 add mall-open-backend-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
