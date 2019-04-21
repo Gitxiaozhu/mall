c1a1d9b5b8efa62fcef95f3c45a4e09c6ca0e2a5
e <849226440@qq.com>
2019-04-21 17:24:40 +0800

    "第二"

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker run -it --rm -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven mvn clean install
-VOLUME /tmp
-add mall-0.0.1-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+cp mall-0.0.1-SNAPSHOT.jar app.jar
+CMD ["java","-jar","/app.jar"]
