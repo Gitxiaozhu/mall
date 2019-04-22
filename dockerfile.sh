dcc7f030a363c3dde6a2174b2efc56b6f21985b8
e <849226440@qq.com>
2019-04-22 20:19:31 +0800

    "第二"
    Signed-off-by: e <849226440@qq.com>

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-docker run -it --rm -v "$(pwd)":/usr/src/mymaven &&\
---link mysql:mysql --link redis:redis &&\
--w /usr/src/mymaven maven mvn clean install
-# cp mall-0.0.1-SNAPSHOT.jar app.jar
-# docker run -d -p 8081:8081 --name springboot --link mysql:mysql --link redis:redis java java -jar /"$(pwd)"/app.jar
+docker run -it --rm -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven --link mysql:mysql --link redis:redis maven mvn clean install
+cp mall-0.0.1-SNAPSHOT.jar app.jar
+docker run -d -p 8081:8081 --name springboot java java -jar /"$(pwd)"/app.jar
