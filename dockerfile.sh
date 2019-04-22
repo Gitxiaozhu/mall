e99711cd3999d65edb788b6d007a27b2447cda36
e <849226440@qq.com>
2019-04-22 15:15:37 +0800

    "第二"

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker run -it --rm -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven mvn clean install
+docker run -it --rm -v "$(pwd)":/usr/src/mymaven &&\
+--link mysql:mysql --link redis:redis &&\
+-w /usr/src/mymaven maven mvn clean install
 # cp mall-0.0.1-SNAPSHOT.jar app.jar
 # docker run -d -p 8081:8081 --name springboot --link mysql:mysql --link redis:redis java java -jar /"$(pwd)"/app.jar
