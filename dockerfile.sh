304b2133187865f2c63576ebb0e85417c61287b9
e <849226440@qq.com>
2019-04-25 22:12:13 +0800

    "第二"
    Signed-off-by: e <849226440@qq.com>

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cd /var/jenkins_home/workspace/mall
+
+echo "$(pwd)"
 docker run --rm -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven mvn clean install
 cp "$(pwd)"/target/mall-0.0.1-SNAPSHOT.jar app.jar
 docker run -p 8081:8081 --name mall -v "$(pwd)"/app.jar:/usr/app.jar --link mysql:mysql --link redis:redis java java -jar /usr/app.jar
