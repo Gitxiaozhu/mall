f34323c212b9813eeba74739aa379c6589ecabb7
e <849226440@qq.com>
2019-06-08 00:11:39 +0800

    "测试reids"
    Signed-off-by: e <849226440@qq.com>

--- dockerfile.sh
+++ dockerfile.sh
@@ -5,10 +5,11 @@ if [ -e app.jar ]; then
 fi
 
 
-docker run -it --rm -v $PWD:/usr/src/java -w /usr/src/java maven mvn clean install -DarchetypeCatalog=internal
+#docker run -it --rm -v $PWD:/usr/src/java -w /usr/src/java maven mvn clean install -DarchetypeCatalog=internal
 # docker run --rm -v "$(pwd)":/usr/src/mymaven -v /var/maven-repo:/usr/share/maven/ref -w /usr/src/mymaven maven mvn clean install -DarchetypeCatalog=internal
 #docker run -it --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "$PWD/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven mvn clean package -DarchetypeCatalog=internal
-echo $PWD
-cp -r "$(pwd)"/target/mall-0.0.1-SNAPSHOT.jar app.jar
-docker run -d -p 8081:8081 --name mall -v app.jar:/usr/app.jar --link mysql:mysql --link redis:redis java java -jar /usr/app.jar
+docker run --rm -v "$(pwd)":/usr/src/mymaven -v /var/maven-repo:/usr/share/maven/ref -w /usr/src/mymaven maven mvn clean install
 
+cp "$(pwd)"/target/mall-0.0.1-SNAPSHOT.jar app.jar
+
+docker run -p 8081:8081 --name mall -v "$(pwd)"/app.jar:/usr/app.jar --link mysql:mysql --link redis:redis java java -jar /usr/app.jar
