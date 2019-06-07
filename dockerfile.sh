9573d4118b8ab06d8c79c94d673916f8459b3696
e <849226440@qq.com>
2019-06-08 00:00:04 +0800

    "测试reids"
    Signed-off-by: e <849226440@qq.com>

--- dockerfile.sh
+++ dockerfile.sh
@@ -3,7 +3,7 @@ if [ -e app.jar ]; then
        docker stop mall
        docker rm mall
 fi
-cp -r "$(pwd)"/target/mall-0.0.1-SNAPSHOT.jar demo.jar
+cp -r "$(pwd)"/target/mall-0.0.1-SNAPSHOT.jar app.jar
 docker run -it --rm -v $PWD:/usr/src/java -w /usr/src/java maven mvn clean install -DarchetypeCatalog=internal
 #docker run --rm -v "$(pwd)":/usr/src/mymaven -v /var/maven-repo:/usr/share/maven/ref -w /usr/src/mymaven maven mvn clean install -DarchetypeCatalog=internal
 #docker run -it --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "$PWD/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven mvn clean package -DarchetypeCatalog=internal
