6031d6336a3c49adbaa6e4aa3e900bb955adc627
e <849226440@qq.com>
2019-06-07 23:33:51 +0800

    "测试reids"
    Signed-off-by: e <849226440@qq.com>

--- dockerfile.sh
+++ dockerfile.sh
@@ -3,8 +3,9 @@ if [ -e app.jar ]; then
        docker stop mall
        docker rm mall
 fi
- cp -r "$(pwd)"/target/mall-0.0.1-SNAPSHOT.jar app.jar
+ cp -r "$(pwd)"/target/mall-0.0.1-SNAPSHOT.jar demo.jar
 docker run --rm -v "$(pwd)":/usr/src/mymaven -v /var/maven-repo:/usr/share/maven/ref -w /usr/src/mymaven maven mvn clean install -DarchetypeCatalog=internal
 #docker run -it --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "$PWD/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven mvn clean package -DarchetypeCatalog=internal
-docker run -d -p 8081:8081 --name mall -v app.jar:/usr/app.jar --link mysql:mysql --link redis:redis java java -jar /usr/app.jar
-
+#docker run -d -p 8081:8081 --name mall -v app.jar:/usr/app.jar --link mysql:mysql --link redis:redis java java -jar /usr/app.jar
+EXPOSE 8081
+ENTRYPOINT ["java","-jar","demo.jar"]
