0354f01a93ae31dd0b599908f30b6c4a99cfdd6a
e <849226440@qq.com>
2019-06-08 12:31:26 +0800

    "测试reids"
    Signed-off-by: e <849226440@qq.com>

--- dockerfile.sh
+++ dockerfile.sh
@@ -10,6 +10,8 @@ fi
 docker run -it --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -v "$PWD/target:/usr/src/mymaven/target" -w /usr/src/mymaven maven mvn clean package -DarchetypeCatalog=internal
 #docker run --rm -v "$(pwd)":/usr/src/mymaven -v /var/maven-repo:/usr/share/maven/ref -w /usr/src/mymaven maven mvn clean install
 
+
+
 #cp "$(pwd)"/target/mall-0.0.1-SNAPSHOT.jar app.jar
 
 docker run -d -p 8081:8081 --name mall -v "$(pwd)"/target/mall-0.0.1-SNAPSHOT.jar:/usr/app.jar --link mysql:mysql --link redis:redis java java -jar /usr/app.jar
